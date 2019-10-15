0dac81998282fa885ac325b536d229e2cc12ff99
👾 Brad 👾 <brad.cypert@gmail.com>
2019-10-14 20:55:58 -0400

    logic for freight and get them working

--- src/tasks/creep/Mine.ts
+++ src/tasks/creep/Mine.ts
@@ -58,23 +58,34 @@ export default class Mine extends Task {
     }
 
     public dropOffEnergy(): void {
-        const dropoff = this.creep.room.find(FIND_STRUCTURES, {filter: s =>
-            (s.structureType === STRUCTURE_CONTAINER && s.store.energy < s.storeCapacity)
-            ||  (s.structureType === STRUCTURE_SPAWN && s.energy < s.energyCapacity)
-            ||  (s.structureType === STRUCTURE_EXTENSION && s.energy < s.energyCapacity)
-            ||  (s.structureType === STRUCTURE_TOWER && s.energy < s.energyCapacity)});
+        const storage = this.creep.room.find(FIND_STRUCTURES, 
+            {filter: s => (s.structureType === STRUCTURE_STORAGE && s.store.energy < s.storeCapacity)
+                || (s.structureType === STRUCTURE_SPAWN && s.energy < s.energyCapacity)});
 
-        if (dropoff.length > 0) {
-            if (this.creep.transfer(dropoff[0], RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
-                this.creep.moveTo(dropoff[0], {visualizePathStyle: {stroke: '#ffffff'}});
+        if (storage.length > 0) {
+            if (this.creep.transfer(storage[0], RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+                this.creep.moveTo(storage[0], {visualizePathStyle: {stroke: '#ffffff'}});
             }
         } else {
-            // Controllers are unique
-            const controller = this.creep.room.find(FIND_STRUCTURES)
-                .filter(s => s.structureType === STRUCTURE_CONTROLLER);
-            if (controller[0]) {
-                if (this.creep.upgradeController(controller[0] as StructureController) === ERR_NOT_IN_RANGE) {
-                    this.creep.moveTo(controller[0], {visualizePathStyle: {stroke: '#ffffff'}});
+            // IF STORAGE ISNT AVAILABLE, DEFAULT BACK TO BASE BEHAVIOR
+            const dropoff = this.creep.room.find(FIND_STRUCTURES, {filter: s =>
+                (s.structureType === STRUCTURE_CONTAINER && s.store.energy < s.storeCapacity)
+                ||  (s.structureType === STRUCTURE_SPAWN && s.energy < s.energyCapacity)
+                ||  (s.structureType === STRUCTURE_EXTENSION && s.energy < s.energyCapacity)
+                ||  (s.structureType === STRUCTURE_TOWER && s.energy < s.energyCapacity)});
+
+            if (dropoff.length > 0) {
+                if (this.creep.transfer(dropoff[0], RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+                    this.creep.moveTo(dropoff[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                }
+            } else {
+                // Controllers are unique
+                const controller = this.creep.room.find(FIND_STRUCTURES)
+                    .filter(s => s.structureType === STRUCTURE_CONTROLLER);
+                if (controller[0]) {
+                    if (this.creep.upgradeController(controller[0] as StructureController) === ERR_NOT_IN_RANGE) {
+                        this.creep.moveTo(controller[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                    }
                 }
             }
         }
